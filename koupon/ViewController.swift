46a56f18095c5196063d88d67d4bee18c96050e6
Tejas Manohar <me@tejas.io>
2015-08-02 19:48:07 -0400

    scaffold app

---
+++ koupon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  koupon
+//
+//  Created by Tejas Manohar on 8/2/15.
+//  Copyright (c) 2015 Tejas Manohar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
